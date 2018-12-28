9481cb4b82e72f5efc1b6937534cf3d0e66665fe
Bharat <bukha@allstate.com>
2018-12-28 20:22:19 +0530

    first commit

---
+++ components/EventList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text,FlatList} from 'react-native';
+
+export default class EventList extends React.Component {
+
+  state  = { events : []}
+
+  componentDidMount(){
+    this.setState({events : require('../db.json').events});
+  }
+
+  render(){
+    return(
+      <FlatList
+        data={this.state.events}
+        renderItem= {({item}) => <Text>{item.title}</Text>}
+        keyExtractor = {item => item.id}
+      />
+    );
+  }
+}
